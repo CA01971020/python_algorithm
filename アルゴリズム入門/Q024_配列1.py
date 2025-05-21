1f12f7a083c223d2bb5fd30fe8471da025458faa
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-05-21 09:51:38 +0900

    Create Q024_配列1.py

---
+++ アルゴリズム入門/Q024_配列1.py
@@ -0,0 +1,13 @@
+numbers = [10, 20, 30, 40, 50]
+i = 0
+
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
+
+print("----------")
+# Pythonらしいコード
+numbers = [10, 20, 30, 40, 50]
+for num in numbers:
+    print(num)
+
