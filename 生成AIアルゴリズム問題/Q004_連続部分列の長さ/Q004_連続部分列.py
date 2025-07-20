0e87645950dd0e1774b4e25d63a87200d5cbb829
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-07-21 01:53:05 +0900

    Update Q004_連続部分列.py

--- 生成AIアルゴリズム問題/Q004_連続部分列の長さ/Q004_連続部分列.py
+++ 生成AIアルゴリズム問題/Q004_連続部分列の長さ/Q004_連続部分列.py
@@ -1,1 +1,14 @@
 sequence = list(map(int, "1 1 0 1 1 1 0 0 1".split()))
+
+count = 0
+max_count = 0
+
+for i in sequence:
+    if i == 1:
+        count += 1
+        if count > max_count :
+            max_count = count
+    else:
+        count = 0
+
+print(max_count)
