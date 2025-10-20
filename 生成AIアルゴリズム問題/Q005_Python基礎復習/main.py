983aeee22cb5ab1f2bb563a4c28770ea3985d774
CA01971020 <107534418+CA01971020@users.noreply.github.com>
2025-10-20 23:47:25 +0900

    Create main.py

---
+++ 生成AIアルゴリズム問題/Q005_Python基礎復習/main.py
@@ -0,0 +1,20 @@
+# 初級問題：偶数カウント
+
+nums = [1 , 2 ,3 , 4 , 5 , 6]
+count = 0
+
+for i in nums :
+    if i % 2 == 0 :
+        count += 1
+print(count)
+
+# Pythonic
+# def count_even(nums):
+#     return sum(1 for n in nums if n % 2 == 0)
+
+# nums = [1, 2, 3, 4, 5, 6]
+# print(count_even(nums))
+
+print("----------")
+
+# 中級：文字列反転
